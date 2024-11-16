93c9045a6d0c36b2886b77746c89a57860e703a6
alice-frln <alice.frelin@edu.univ-fcomte.fr>
2024-11-16 21:34:13 +0100

    class noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 // On cible les éléments à modifier
 const toggle =document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu")
+const bodyElement = document.body;
 
 // Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
 
@@ -10,4 +11,12 @@ toggle.addEventListener("click", () => {
   nav.ariaHidden = isOpen
 toggle.ariaExpanded = isClosed
 console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+if( toggle.ariaExpanded == "true")
+{
+  bodyElement.classList.add("noscroll");
+}
+else
+{
+  bodyElement.classList.remove("noscroll");
+}
 })
